3bb0c51c27877c90e4222b04d8d502da90db00d5
Youssif Hany <youssifhany962@gmail.com>
2021-12-27 18:11:07 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Youssif Hany on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
